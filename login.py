0c0b0c2264e6a699011789e9a76b65f0585cc434
misteralen <alen.sultanov@gmail.com>
2022-11-22 11:14:47 +0600

    Creating separated file for function loginning into Moodle Parsing all subjects and links

---
+++ login.py
@@ -0,0 +1,77 @@
+import pickle
+import time
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+# from fake_useragent import UserAgent
+
+
+def auth_microsoft(barcode, password):
+    options = webdriver.ChromeOptions()
+    # options.add_argument(f"user-agent={UserAgent().chrome}")
+    options.add_argument(
+        "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/107.0.0.0 Safari/537.36")
+    options.add_argument("--headless")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    browser = webdriver.Chrome(options=options)
+
+    email_field = (By.ID, "i0116")
+    password_field = (By.ID, "i0118")
+    next_button = (By.ID, "idSIButton9")
+    browser.get('https://moodle.astanait.edu.kz/auth/oidc/')
+    WebDriverWait(browser, 10).until(ec.element_to_be_clickable(email_field)).send_keys(f""
+                                                                                        f""
+                                                                                        f""
+                                                                                        f""
+                                                                                        f""
+                                                                                        f""
+                                                                                        f""
+                                                                                        f""
+                                                                                        f""
+                                                                                        f""
+                                                                                        f""
+                                                                                        f""
+                                                                                        f"{barcode}@astanait.edu.kz")
+    WebDriverWait(browser, 10).until(ec.element_to_be_clickable(next_button)).click()
+    WebDriverWait(browser, 10).until(ec.element_to_be_clickable(password_field)).send_keys(password)
+    WebDriverWait(browser, 10).until(ec.element_to_be_clickable(next_button)).click()
+    WebDriverWait(browser, 10).until(ec.element_to_be_clickable(next_button)).click()
+
+    pickle.dump(browser.get_cookies(), open(f"cookies/{barcode}", "wb"))
+    browser_cookies = browser.get_cookies()
+    browser.close()
+    browser.quit()
+
+
+def auth_with_cookies(barcode):
+    session = requests.Session()
+    cookies = {}
+    for cookie in pickle.load(open(f"cookies/{barcode}", 'rb')):
+        cookies[cookie['name']] = cookie['value']
+    response = session.get('https://moodle.astanait.edu.kz/?lang=en', cookies=cookies)
+    if "You are not logged in" in response.text:
+        print("Cookies are invalid. Trying to log in again...")
+    else:
+        print("We are authorized.")
+        return response.text
+
+
+class SignIn:
+    main_url = "https://moodle.astanait.edu.kz/"
+
+    def __init__(self, data):
+        self.barcode = data['barcode']
+        self.password = data['password']
+
+    def login_moodle(self):
+        try:
+            return auth_with_cookies(self.barcode)
+        except FileNotFoundError:
+            auth_microsoft(self.barcode, self.password)
+            return auth_with_cookies(self.barcode)
--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 from environs import Env
-import pickle
-import time
-import requests
-from fake_useragent import UserAgent
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support import expected_conditions as ec
-from selenium.webdriver.support.ui import WebDriverWait
+from login import SignIn
+from bs4 import BeautifulSoup
 
 
 def load_config(path: str = None):
@@ -20,49 +13,16 @@ def load_config(path: str = None):
     return data
 
 
-def login_moodle(browser, data):
-    email_field = (By.ID, "i0116")
-    password_field = (By.ID, "i0118")
-    next_button = (By.ID, "idSIButton9")
-
-    browser.get('https://moodle.astanait.edu.kz/auth/oidc/')
-
-    WebDriverWait(browser, 10).until(ec.element_to_be_clickable(email_field)).send_keys(f""
-                                                                                        f""
-                                                                                        f""
-                                                                                        f"{data['barcode']}@astanait"
-                                                                                        f".edu.kz")
-    WebDriverWait(browser, 10).until(ec.element_to_be_clickable(next_button)).click()
-    WebDriverWait(browser, 10).until(ec.element_to_be_clickable(password_field)).send_keys(data['password'])
-    WebDriverWait(browser, 10).until(ec.element_to_be_clickable(next_button)).click()
-    WebDriverWait(browser, 10).until(ec.element_to_be_clickable(next_button)).click()
-    time.sleep(5)
-
-    pickle.dump(browser.get_cookies(), open(f"{data['barcode']}_cookies", "wb"))
-    browser_cookies = browser.get_cookies()
-    browser.close()
-    browser.quit()
-
-
-def login_with_cookies(browser, data):
-    session = requests.Session()
-    cookies = {}
-    for cookie in pickle.load(open(f"{data['barcode']}_cookies", 'rb')):
-        cookies[cookie['name']] = cookie['value']
-    response = session.get('https://moodle.astanait.edu.kz/', cookies=cookies)
-    print(response.text)
-
-
 def main():
     data = load_config(".env")
-    options = Options()
-    # options.add_argument(f"user-agent={UserAgent().chrome}")
-    # options.add_argument("--disable-extensions")
-    # options.add_argument("--headless")
-    browser = webdriver.Chrome(options=options)
-
-    # login_moodle(browser, data)
-    login_with_cookies(browser, data)
+    signin = SignIn(data)
+    src = signin.login_moodle()
+    soup = BeautifulSoup(src, 'lxml')
+    text = soup.find_all('a')
+    for el in text:
+        if el.get('data-parent-key') == 'mycourses':
+            print(f"ID of subject - {el.get('data-key')}")
+            print(f"Link - {el.get('href')}")
 
 
 if __name__ == '__main__':
