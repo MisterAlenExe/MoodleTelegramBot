a5e0707d428e4bf707cdad64402a51f1ed593b3c
misteralen <alen.sultanov@gmail.com>
2022-12-15 16:59:56 +0600

    closes #3

--- functions/parser.py
+++ functions/parser.py
@@ -1,4 +1,5 @@
 import datetime
+import aiohttp
 import requests
 import json
 from bs4 import BeautifulSoup
@@ -12,12 +13,12 @@ class Parser:
     course_url = "https://moodle.astanait.edu.kz/course/view.php?id="
 
     async def get_token_and_userid(self, cookies):
-        session = requests.Session()
-        src = session.get(self.security_url, cookies=cookies)
-        soup = BeautifulSoup(src.text, 'lxml')
-        token = soup.find('tbody').find('tr').find('td').text
-        userid = soup.find('div', class_='popover-region collapsed popover-region-notifications').get('data-userid')
-        return token, userid
+        async with aiohttp.ClientSession(cookies=cookies) as session:
+            async with session.get(self.security_url) as response:
+                soup = BeautifulSoup(await response.text(), 'lxml')
+                token = soup.find('tbody').find('tr').find('td').text
+                userid = soup.find('div', class_='popover-region collapsed popover-region-notifications').get('data-userid')
+                return token, userid
 
     async def get_courses(self, webservice_token, user_id):
         args = {
@@ -26,22 +27,24 @@ class Parser:
             'wsfunction': 'core_enrol_get_users_courses',
             'userid': user_id
         }
-        response = requests.get(self.api_url, params=args)
-        text = json.loads(response.text)
-        courses = {}
 
-        for course in text:
-            id_subject = course['id']
-            name_subject = course['fullname']
-            link_subject = f"{self.course_url}{id_subject}"
-            courses.update({
-                id_subject: {
-                    'id': id_subject,
-                    'name': name_subject,
-                    'link': link_subject
-                }
-            })
-        return courses
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.api_url, params=args) as response:
+                text = json.loads(await response.text())
+                courses = {}
+
+                for course in text:
+                    id_subject = course['id']
+                    name_subject = course['fullname']
+                    link_subject = f"{self.course_url}{id_subject}"
+                    courses.update({
+                        id_subject: {
+                            'id': id_subject,
+                            'name': name_subject,
+                            'link': link_subject
+                        }
+                    })
+                return courses
 
     async def get_grades(self, id_subject, webservice_token, user_id):
         args = {
@@ -51,21 +54,23 @@ class Parser:
             'userid': user_id,
             'courseid': id_subject
         }
-        response = requests.get(self.api_url, params=args)
-        text = json.loads(response.text)['tables'][0]['tabledata']
-        grades = {}
-        for el in text:
-            if el.__class__ is list or len(el) in [0, 2]:
-                continue
-            itemname = BeautifulSoup(el['itemname']['content'], 'lxml').text
-            grade = el['grade']['content']
-            strings = ['Включая незаполненные оценки.', '(not to edit)', 'Include empty grades.']
-            for string in strings:
-                itemname = itemname.replace(string, '')
-            grades.update({
-                itemname: grade
-            })
-        return grades
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.api_url, params=args) as response:
+                text = json.loads(await response.text())['tables'][0]['tabledata']
+                grades = {}
+                for el in text:
+                    if el.__class__ is list or len(el) in [0, 2]:
+                        continue
+                    itemname = BeautifulSoup(el['itemname']['content'], 'lxml').text
+                    grade = el['grade']['content']
+                    strings = ['Включая незаполненные оценки.', '(not to edit)', 'Include empty grades.']
+                    for string in strings:
+                        itemname = itemname.replace(string, '')
+                    grades.update({
+                        itemname: grade
+                    })
+                return grades
 
     async def get_deadlines(self, webservice_token):
         args = {
@@ -73,29 +78,29 @@ class Parser:
             'wstoken': webservice_token,
             'wsfunction': 'mod_assign_get_assignments'
         }
-        response = json.loads(requests.get(self.api_url, params=args).text)
 
-        time_now = datetime.datetime.now().timestamp()
-
-        deadlines = {}
-
-        for course in response['courses']:
-            deadlines.update({
-                course['id']: {}
-            })
-            for assign in course['assignments']:
-                # duedate = datetime.datetime.fromtimestamp(assign['duedate']).replace(microsecond=0)
-                duedate = assign['duedate']
-                if duedate > time_now:
-                    id_assign = assign['cmid']
-                    name = assign['name']
-                    deadline = duedate
-                    deadlines[course['id']].update({
-                        id_assign: {
-                            'name': name,
-                            'deadline': deadline,
-                            'link': f"{self.assign_url}{id_assign}"
-                        }
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.api_url, params=args) as response:
+                text = json.loads(await response.text())
+                time_now = datetime.datetime.now().timestamp()
+                deadlines = {}
+                for course in text['courses']:
+                    deadlines.update({
+                        course['id']: {}
                     })
+                    for assign in course['assignments']:
+                        # duedate = datetime.datetime.fromtimestamp(assign['duedate']).replace(microsecond=0)
+                        duedate = assign['duedate']
+                        if duedate > time_now:
+                            id_assign = assign['cmid']
+                            name = assign['name']
+                            deadline = duedate
+                            deadlines[course['id']].update({
+                                id_assign: {
+                                    'name': name,
+                                    'deadline': deadline,
+                                    'link': f"{self.assign_url}{id_assign}"
+                                }
+                            })
 
-        return deadlines
+                return deadlines
--- tgbot/handlers/notify.py
+++ tgbot/handlers/notify.py
@@ -67,4 +67,4 @@ def register_schedulers(bot, scheduler):
     now = datetime.datetime.now()
     time_start = (now.replace(second=0, microsecond=0) + datetime.timedelta(minutes=10 - now.minute % 10)).strftime(
         "%Y-%m-%d %H:%M:%S")
-    scheduler.add_job(auto_update, 'interval', minutes=10, start_date='2022-12-15 16:03:20', kwargs={'bot': bot})
+    scheduler.add_job(auto_update, 'interval', minutes=10, start_date=time_start, kwargs={'bot': bot})
